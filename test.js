8580627a944a5f6aaaee8acdefc66b2723cd1b88
Joe Butler <jbu4email@gmail.com>
2019-10-29 15:40:38 +0000

    Break Character HP

--- test.js
+++ test.js
@@ -8,5 +8,14 @@ function test () {
 	console.assert(character.level === 5);
 	console.assert(character.getAttackStat() === 5);
 	console.assert(character.getCurrentHP() === character.getMaxHPStat());
+
+
+	character.setHP(200)
+	console.log("CurrentHP:" + character.getCurrentHP());
+	console.log("MaxHP:" + character.getMaxHPStat());
+	console.assert(character.getCurrentHP() === character.getMaxHPStat());
+
+
+
 	console.log(character.getStatisticsString());
 }
