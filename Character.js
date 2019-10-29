39c1c9583c5b05f7d713f58b8a2906be2577c6b7
Joe Butler <jbu4email@gmail.com>
2019-10-29 13:20:08 +0000

    Update Character.js

--- Character.js
+++ Character.js
@@ -3,4 +3,13 @@ class Character {
 	constructor () {
 
 	}
+
+	getGeneralStatisticsBasedOnLevel (level) {
+
+		return {
+			hp: level * 15,
+			attack: level * 1,
+			defence: level * 0.75
+		}
+	}
 }
